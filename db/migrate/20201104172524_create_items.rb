6006ff8a64eaefac759b7dc39318053e93764abd
Vova <451585@gmail.com>
2020-11-04 20:41:48 +0100

    Add index and column

--- db/migrate/20201104172524_create_items.rb
+++ db/migrate/20201104172524_create_items.rb
@@ -2,10 +2,14 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.float :price
-      t.string :nane
+      t.string :name
       t.boolean :real
       t.float :weight
+      t.string :description
       t.timestamps
     end
+
+    add_index :items, :price
+    add_index :items, :name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,14 @@ ActiveRecord::Schema.define(version: 2020_11_04_172524) do
 
   create_table "items", force: :cascade do |t|
     t.float "price"
-    t.string "nane"
+    t.string "name"
     t.boolean "real"
     t.float "weight"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_items_on_name"
+    t.index ["price"], name: "index_items_on_price"
   end
 
 end
