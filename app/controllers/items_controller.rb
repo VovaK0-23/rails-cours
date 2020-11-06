86614af4a3dbf2f120cdcbb4a10e01992775a9f7
Vova <451585@gmail.com>
2020-11-06 23:40:01 +0100

    update

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,10 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    render body: @items.map  {|i| "#{i.name}:#{i.price}:"}
+  end
+
+  def create
+    render body: 'created'
+  end
+end
