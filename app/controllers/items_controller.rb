2bc32f0212dce3f539819733eb79703fe258e467
Vova <451585@gmail.com>
2020-11-22 15:23:08 +0100

    21.11

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,11 +23,21 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit;
+
+  end
 
   def update; end
 
-  def destroy; end
+  def destroy
+    item = Item.where(id: params[:id]).first.destroy
+
+    if item.destroyed?
+      redirect_to items_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
 
   private
   def items_params
