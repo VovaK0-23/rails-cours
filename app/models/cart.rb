8bc089dff95022cc2bdf43f351c2f9a02c9280e6
Vova <451585@gmail.com>
2020-11-24 18:56:17 +0100

    has_many through

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,8 @@
 class Cart < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional:  true
+  has_many   :positions
+  has_many   :items, through: :positions
+
+  # TODO: for carts_items
+  # has_and_belongs_to_many :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_150812) do
+ActiveRecord::Schema.define(version: 2020_11_24_170627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2020_11_24_150812) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "positions", force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "item_id"
+    t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
