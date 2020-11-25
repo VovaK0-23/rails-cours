a8dea71d16a20490a0b70dc8e9e68d07d14e6410
Vova <451585@gmail.com>
2020-11-25 21:34:47 +0100

    eager loading

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,7 @@ class ItemsController < ApplicationController
   after_action  :show_info, only: %i[index]
 
   def index
-    @items = Item
-    @items = @items.where('price >= ?', params[:price_from])       if params[:price_from]
-    @items = @items.where('created_at >= ?', 1.day.ago)            if params[:today]
-    @items = @items.where('votes_count >= ?', params[:votes_from]) if params[:votes_from]
-    @items = @items.order(:id)
+    @items = Item.all
   end
 
   def create
