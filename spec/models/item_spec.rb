db705657b1a2634b52f282bb2e38017eed151ac8
Vova <451585@gmail.com>
2020-11-28 20:39:23 +0100

    rspec test

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  it { should validate_presence_of :name }
+  it { should validate_numericality_of :price }
+  it { should have_many :positions }
+  it { should have_many :comments }
+  it { should have_many :carts }
+  it { should have_one :image }
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 
 require 'spec_helper'
+require 'shoulda/matchers'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
@@ -21,4 +23,11 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
